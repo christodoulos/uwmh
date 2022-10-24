496639e8d21ffcaea8aa4eeea3cc55db14fc9793
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-10-24 18:45:54 +0300

    nest serve static

--- apps/attica-dt-api/src/app/app.module.ts
+++ apps/attica-dt-api/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,9 +9,15 @@ import { LocationModule } from './location/location.module';
 import { MultiBoundaryModule } from './multi_boundary/multi_boundary.module';
 import { MultilineModule } from './multi_line/multi_line.module';
 
+import { join } from 'path';
+
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATABASE_URI),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'gnosys'),
+      exclude: ['/api*'],
+    }),
     LocationModule,
     MultiBoundaryModule,
     MultilineModule,
