27a24d56a467395e0cae29cee25dc870ffb4c00b
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-11-21 17:17:22 +0200

    mongoose prod

--- apps/attica-dt-api/src/main.ts
+++ apps/attica-dt-api/src/main.ts
@@ -7,8 +7,8 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
 import { AppModule } from './app/app.module';
-import * as mongoose from 'mongoose';
-mongoose.set('debug', true);
+// import * as mongoose from 'mongoose';
+// mongoose.set('debug', true);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
