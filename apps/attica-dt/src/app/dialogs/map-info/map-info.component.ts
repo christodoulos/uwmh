5e21e812da291278b2a6c7ff24f0613841acb822
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-11-27 19:00:51 +0200

    plc excel export

--- apps/attica-dt/src/app/dialogs/map-info/map-info.component.ts
+++ apps/attica-dt/src/app/dialogs/map-info/map-info.component.ts
@@ -13,6 +13,8 @@ import { MatSort } from '@angular/material/sort';
 import { PNPLCEntities, PNWeatherRepository } from '../../state';
 import { PNPLC } from '@uwmh/data';
 
+import * as XLSX from 'xlsx';
+
 @Component({
   selector: 'uwmh-map-info',
   templateUrl: './map-info.component.html',
@@ -37,6 +39,7 @@ export class MapInfoComponent implements OnInit, AfterViewInit {
   @ViewChild(MatSort) sort!: MatSort;
   weatherData$ = this.pnWeather.weatherData$;
   plcEntities$ = this.pnPLC.allEntities$;
+  entities: PNPLC[] = [];
 
   constructor(
     private pnWeather: PNWeatherRepository,
@@ -44,13 +47,35 @@ export class MapInfoComponent implements OnInit, AfterViewInit {
   ) {}
 
   ngOnInit(): void {
-    this.plcEntities$.subscribe(
-      (entities) => (this.dataSource.data = entities)
-    );
+    this.plcEntities$.subscribe((entities) => {
+      this.dataSource.data = entities.sort(() => {
+        return -1;
+      });
+      this.entities = entities;
+    });
   }
 
   ngAfterViewInit(): void {
     this.dataSource.sort = this.sort;
     this.dataSource.paginator = this.paginator;
+  }
+
+  exportExcell(): void {
+    const excelobj = this.entities.map((item) => ({
+      timestamp: item.ts,
+      'Temperature membrane tank 5': item.col3,
+      'LT1 (pH membrane tank 5)': item.col4,
+      'DO ppm LDO aeriation tank 4A': item.col5,
+      'DO ppm anoxic tank3': item.col6,
+      'MLSS SOLID mg/l membrane tank 5': item.col7,
+      'MLSS SOLID mg/l membrane tank 4A': item.col8,
+      'LDO DO ppm anoxic': item.col9,
+      'Temperature anoxic tank': item.col10,
+      'Turbidity NTU tank 10': item.col11,
+    }));
+    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(excelobj);
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+    XLSX.writeFileXLSX(wb, 'plant_nursery_plc.xlsx');
   }
 }
