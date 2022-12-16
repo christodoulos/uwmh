7d99dd869bd221233f23f3995878dcb3edb54101
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2022-12-16 10:14:07 +0200

    state housekeeping

--- libs/state/src/index.ts
+++ libs/state/src/index.ts
@@ -1,21 +1,4 @@
 // export all the interfaces
-export * from './lib/interfaces/backend';
-export * from './lib/interfaces/layer';
-export * from './lib/interfaces/source';
-export * from './lib/interfaces/pnweather';
-export * from './lib/interfaces/mapwhere';
-export * from './lib/interfaces/pnplc';
-export * from './lib/interfaces/googleauth';
-export * from './lib/interfaces/user';
-export * from './lib/interfaces/dto';
+export * from './lib/interfaces';
 // export all the repositories
-export * from './lib/repositories/backend.service';
-export * from './lib/repositories/draw';
-export * from './lib/repositories/layers';
-export * from './lib/repositories/mapwhere';
-export * from './lib/repositories/mapwhere';
-export * from './lib/repositories/sources';
-export * from './lib/repositories/ui';
-export * from './lib/repositories/user';
-export * from './lib/repositories/pnplc';
-export * from './lib/repositories/pnweather';
+export * from './lib/repositories';
